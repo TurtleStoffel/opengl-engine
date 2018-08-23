37132e51b18c44636ce12911a17464f59d0f0aee
Stefan <stefanwauters@gmail.com>
2018-08-23 12:52:29 +0200

    Fix application interface

--- include/application.hpp
+++ include/application.hpp
@@ -1,22 +1,35 @@
 #ifndef APPLICATION_HPP
 #define APPLICATION_HPP
 
+#include <glm/glm.hpp>
+
 #include "opengl.hpp"
+
+class Scene;
 
 class Application {
    public:
-    Application();
+    Application(SDL_Window* pWindow);
     ~Application();
+
     void run();
+    void setScene(Scene* pScene);
 
    private:
-    bool _stop = false;
-    int _windowWidth = 800;
-    int _windowHeight = 600;
-    const int FPS = 30;
+    bool _handleInput(SDL_Event event);
 
+    bool _running = true;
+
+    Scene* _pScene;
     SDL_Window* _pWindow;
     GLuint _shader;
+
+    int _windowWidth;
+    int _windowHeight;
+
+    glm::vec3 _cameraPosition  = glm::vec3(0.0f, -5.0f, 10.0f);
+    glm::vec3 _cameraDirection = glm::vec3(0.0f, 0.8f, -1.0f);
+    glm::vec3 _cameraUp        = glm::vec3(0.0f, 1.0f, 0.0f);
 };
 
 #endif
---
+++ include/const.hpp
@@ -0,0 +1,10 @@
+#ifndef CONST_HPP
+#define CONST_HPP
+
+namespace constant {
+const int initialWindowWidth  = 800;
+const int initialWindowHeight = 600;
+const int FPS                 = 30;
+}  // namespace constant
+
+#endif
