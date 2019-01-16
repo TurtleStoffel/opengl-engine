29c82d5337154020cecd4d6a65448a403e9f1095
Stefan <stefanwauters@gmail.com>
2019-01-16 20:38:42 +0100

    GW-5 let intersection take into account actual position of object

--- include/objects/transform.hpp
+++ include/objects/transform.hpp
@@ -13,11 +13,14 @@ class Transform {
     void translate(glm::vec3 v);
     void rotate(float degrees);
 
+    glm::vec3 getInitialPosition();
     glm::vec3 getPosition();
 
     void passModelMatrixToShader();
 
    private:
+    glm::mat4 _calculateModelMatrix();
+
     glm::vec3 _position = glm::vec3(0.0f, 0.0f, 0.0f);
     glm::vec3 _scale    = glm::vec3(1.0f, 1.0f, 1.0f);
     float _rotation     = 0.0f;
--- src/objects/transform.cpp
+++ src/objects/transform.cpp
@@ -16,15 +16,26 @@ void Transform::rotate(float degrees) {
     _rotation += degrees;
 }
 
-glm::vec3 Transform::getPosition() {
+glm::vec3 Transform::getInitialPosition() {
     return _position;
 }
 
+glm::vec3 Transform::getPosition() {
+    glm::vec4 transformedPosition = glm::vec4(_position, 0.0f) * _calculateModelMatrix();
+    return glm::vec3(transformedPosition.x, transformedPosition.y, transformedPosition.z);
+}
+
 void Transform::passModelMatrixToShader() {
+    glm::mat4 modelMatrix = _calculateModelMatrix();
+
+    // Apply model transformation matrix
+    ShaderContainer::instance()->setModelViewProjectionMatrix(&modelMatrix[0][0], nullptr, nullptr);
+}
+
+glm::mat4 Transform::_calculateModelMatrix() {
     glm::mat4 modelMatrix = glm::rotate(glm::mat4(1.0f), _rotation, glm::vec3(0.0f, 0.0f, 1.0f));
     modelMatrix           = glm::scale(modelMatrix, _scale);
     modelMatrix           = glm::translate(modelMatrix, _position);
 
-    // Apply model transformation matrix
-    ShaderContainer::instance()->setModelViewProjectionMatrix(&modelMatrix[0][0], nullptr, nullptr);
+    return modelMatrix;
 }
