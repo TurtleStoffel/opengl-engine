9dfe1a11b6f58031f11bb54c31dba69f06a6634c
Stefan <stefanwauters@gmail.com>
2019-04-25 20:29:29 +0200

    GW-103 Generic Properties

---
+++ include/property.hpp
@@ -0,0 +1,40 @@
+#ifndef PROPERTY_HPP
+#define PROPERTY_HPP
+
+#include <functional>
+#include <vector>
+
+template <typename T>
+class Property {
+   public:
+    /**
+     * Add new callback to Property
+     *
+     * Example usage:
+     *   fooProperty.addCallback([this] {
+     *     this->_rerender();
+     *   })
+     */
+    void addCallback(std::function<void()> callback) {
+        _callbacks.push_back(callback);
+    }
+
+    /**
+     * Operator overloading that automatically calls all callbacks when value is changed
+     */
+    T& operator=(const T& other) {
+        _value = other;
+
+        for (std::function<void()> callback : _callbacks) {
+            callback();
+        }
+
+        return _value;
+    }
+
+   private:
+    T _value;
+    std::vector<std::function<void()>> _callbacks;
+};
+
+#endif
