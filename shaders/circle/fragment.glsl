9b151b2c1109b6d4e166e6645a4f3fc4f6b6386e
Stefan Wauters <stefanwauters@gmail.com>
2021-01-20 20:26:01 +0100

    Fix bugs in Circle Shader

--- shaders/circle/fragment.glsl
+++ shaders/circle/fragment.glsl
@@ -1,13 +1,13 @@
 #version 330 core
 out vec4 fColor;
 
-uniform vec3 circlePosition = vec4(0.0, 1.0, 1.0);
+uniform vec3 circlePosition;
 
 flat in vec3 vertexColor;
 in vec4 worldPosition;
 
 void main() {
-    if(distance(worldPosition.xyz, circleLocation) < 0.5) {
+    if(distance(worldPosition.xyz, circlePosition) < 0.5) {
         fColor = vec4(vertexColor*0.3, 1.0);
     } else {
         fColor = vec4(vertexColor, 1.0);
