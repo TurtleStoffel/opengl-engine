404d10552e81e7874c978cd4236d94558fe37e31
Stefan Wauters <stefanwauters@gmail.com>
2021-04-05 16:14:54 +0200

    Add a little bit of noise to Low Poly fragment shader

--- shaders/low-poly/fragment.glsl
+++ shaders/low-poly/fragment.glsl
@@ -7,10 +7,18 @@ uniform vec4 settlementLocation = vec4(0.0, 1.0, 1.0, 1.0);
 flat in vec3 vertexColor;
 in vec4 worldPosition;
 
+
+float rand(vec2 co){
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
 void main() {
+    // Add a little bit of noise to the color
+    vec3 updatedVertexColor = vertexColor + vec3(0.02f, 0.02f, 0.02f)*rand(worldPosition.xy/10);
+
     if(isSettlementEnabled && distance(worldPosition, settlementLocation) < 0.5) {
-        fColor = vec4(vertexColor*0.3, 1.0);
+        fColor = vec4(updatedVertexColor*0.3, 1.0);
     } else {
-        fColor = vec4(vertexColor, 1.0);
+        fColor = vec4(updatedVertexColor, 1.0);
     }
 }
