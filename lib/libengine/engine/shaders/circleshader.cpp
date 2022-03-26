ad14d2cd983e938c170323619bd98cc736ec0621
Stefan Wauters <stefanwauters@gmail.com>
2022-03-26 16:32:15 +0100

    Remove CircleShader

--- lib/libengine/engine/shaders/circleshader.cpp
+++
@@ -1,9 +0,0 @@
-#include "engine/shaders/circleshader.hpp"
-
-CircleShader::CircleShader()
-      : Engine::Shader{"shaders/circle/vertex.glsl", "shaders/circle/fragment.glsl"} {
-}
-
-auto CircleShader::setCirclePosition(const glm::vec3& position) const -> void {
-    glUniform3fv(glGetUniformLocation(m_id, "circlePosition"), 1, &position[0]);
-}
