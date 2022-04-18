c3cf3c974102bdd30db140d199883a1013501e14
Stefan Wauters <stefanwauters@gmail.com>
2022-04-18 18:07:24 +0200

    Remove dead code

--- apps/godworld/src/scene/planetscene.cpp
+++
@@ -1,8 +0,0 @@
-#include "planetscene.hpp"
-
-#include "engine/entities/planet.hpp"
-
-PlanetScene::PlanetScene(Engine::Shaders::Registry& shaderRegistry)
-      : Engine::Scene{shaderRegistry} {
-    addEntity(Engine::Planet::createDefault(0.0f, 3.0f, m_shaderRegistry));
-}
